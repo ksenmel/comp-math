98023883e6e29ca601d0af0778b0193a3a051fc2
ksenmel <ksemelashenko@gmail.com>
2024-03-18 14:11:41 +0300

    add functions to approximate

--- task-1/main.c
+++ task-1/main.c
@@ -3,13 +3,20 @@
 
 #include "parallel.c"
 
-double d2u_2 (double x, double y) { return 6000 * x + 12000 * y; }
+double u_1(double x, double y) { return y * sin(x); }
+double d2u_1 (double x, double y) { return -y * sin (x); }
+
 double u_2(double x, double y) { return 1000 * pow(x, 3) + 2000 * pow(y, 3); }
+double d2u_2 (double x, double y) { return 6000 * x + 12000 * y; }
+
+double u_3(double x, double y) { return exp(x*y); }
+double d2u_3 (double x, double y) { return exp(x*y)*pow(y, 2) + exp(x*y)*pow(x, 2); }
+
 
 int main() {
 
     int size[] = {100, 200, 300, 500, 1000};
-    int threads[] = {1, 4, 8, 12}; // max 12 threads on M2
+    int threads[] = {1, 8};
 
     int lsize = sizeof(size) / sizeof(size[0]);
     int lthreads = sizeof(threads) / sizeof(threads[0]);
@@ -21,6 +28,5 @@ int main() {
             run_parallel(size[n], threads[i], d2u_2, u_2);
         }
     }
-
     return 0;
 }
